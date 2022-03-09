110838d54b7f8b2e3d039051fe0db3b4b4b5f45e
codewithray <raymondadutwum@gmail.com>
2022-03-09 17:26:14 +0000

    added nav links

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use((req, res, next) => {
     let now = new Date().toString()
     let log = `${req.method} ${req.url} ${res.statusCode} ${now} \n`;
     fs.appendFile('server.log', log, err => {
-        if(err) console.log(err);
+        if (err) console.log(err);
     })
     console.log(log)
     next();
 })
-app.use((req, res, next) => {
-    res.render('maintenance.hbs')
-})
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
 
 // hbs config
 hbs.registerPartials(__dirname + '/views/partials')
@@ -32,6 +32,12 @@ app.get('/', (req, res) => {
     res.render('home.hbs', {
         title: 'Home',
         year: new Date().getFullYear()
+    })
+})
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        title: 'Projects'
     })
 })
 
